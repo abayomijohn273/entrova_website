cccc4a2cf8f4bb8ebac0263cce7cec3e6b9c7208
Abayomi Olatunji <abayomijohn273@gmail.com>
2024-02-25 13:24:11 +0100

    feat: nav

--- src/components/layout/header/index.js
+++ src/components/layout/header/index.js
@@ -3,6 +3,14 @@ import Image from 'next/image'
 import Link from 'next/link'
 import Logo from "../../../../public/images/svg/logo.svg"
 
+const NavLink = ({ href, title }) => {
+    return <div className='flex items-center'>
+        <Link href={href} className='text-sm lg:text-sm-15 xl:text-base hover:text-secondary transition-all ease-in-out duration-150'>
+            {title}
+        </Link>
+    </div>
+}
+
 const Header = () => {
     return (
         <header className={`sticky top-0 z-10 w-full overflow-hidden mx-auto bg-white`}>
@@ -15,12 +23,22 @@ const Header = () => {
                         </Link>
                     </div>
 
+                    <div className='hidden md:flex w-7/12  justify-center'>
+                        <div className={`flex items-center space-x-4 lg:space-x-7`}>
+                            <NavLink title='Employees' href='/employees' />
+                            <NavLink title='Contractors' href='/contractors' />
+                            <NavLink title='Integration' href='/integrations' />
+                            <NavLink title='Payroll' href='/payroll' />
+                            <NavLink title='API' href='/api' />
+                        </div>
+                    </div>
+
                     <div className='flex flex-1 justify-end'>
                         <div className={`flex items-center space-x-4`}>
-                            <div className='hidden md:block'>
-                                <LinkButton className="px-8" title={"Book A Demo"} />
-                            </div>
                             <div className=''>
+                                <LinkButton className="px-4 lg:px-8" title={"Book A Demo"} />
+                            </div>
+                            <div className='hidden lg:block'>
                                 <LinkButton title={"Log In"} buttonType='' showArrow />
                             </div>
                         </div>
